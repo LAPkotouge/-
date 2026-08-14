a128065a108093ae94b89f75857aaa99e092217c
LAPkotouge <kotouge@lap.co.jp>
2026-08-14 10:47:04 +0900

    Bump service worker cache to v4 so updated app.js is loaded

--- taikai_voice_pwa_v1/sw.js
+++ taikai_voice_pwa_v1/sw.js
@@ -1,4 +1,4 @@
-const CACHE = "taikai-voice-v3";
+const CACHE = "taikai-voice-v4";
 
 self.addEventListener("install", event => {
   event.waitUntil(
@@ -7,7 +7,7 @@ self.addEventListener("install", event => {
         "./",
         "./index.html",
         "./style.css",
-        "./app/app.js?v=3",
+        "./app/app.js?v=4",
         "./manifest.json"
       ])
     ).then(() => self.skipWaiting())
