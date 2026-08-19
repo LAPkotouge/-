21c7576600a7eff5fc94f3d362756e9b1087f204
LAPkotouge <kotouge@lap.co.jp>
2026-08-19 23:52:09 +0900

    v25: refresh PWA cache

--- taikai_voice_pwa_v1/sw.js
+++ taikai_voice_pwa_v1/sw.js
@@ -1,4 +1,4 @@
-const CACHE = "taikai-voice-v24";
-self.addEventListener("install",event=>{event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(["./","./index.html","./style.css?v=24","./app/app.js?v=22","./manifest.json"])).then(()=>self.skipWaiting()))});
+const CACHE = "taikai-voice-v25";
+self.addEventListener("install",event=>{event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(["./","./index.html","./style.css?v=25","./app/app.js?v=22","./manifest.json"])).then(()=>self.skipWaiting()))});
 self.addEventListener("activate",event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key)))).then(()=>self.clients.claim()))});
 self.addEventListener("fetch",event=>{event.respondWith(caches.match(event.request).then(response=>response||fetch(event.request)))});
