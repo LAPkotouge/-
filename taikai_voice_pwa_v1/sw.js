b095875b178a7fff86e39d2b6cf23dc6fe83c3e2
LAPkotouge <kotouge@lap.co.jp>
2026-08-19 19:51:44 +0900

    Bump PWA cache for approved UI v19

--- taikai_voice_pwa_v1/sw.js
+++ taikai_voice_pwa_v1/sw.js
@@ -1,4 +1,4 @@
-const CACHE = "taikai-voice-v18";
-self.addEventListener("install",event=>{event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(["./","./index.html","./style.css?v=17","./app/app.js?v=16","./manifest.json"])).then(()=>self.skipWaiting()))});
+const CACHE = "taikai-voice-v19";
+self.addEventListener("install",event=>{event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(["./","./index.html","./style.css?v=19","./app/app.js?v=16","./manifest.json"])).then(()=>self.skipWaiting()))});
 self.addEventListener("activate",event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key)))).then(()=>self.clients.claim()))});
 self.addEventListener("fetch",event=>{event.respondWith(caches.match(event.request).then(response=>response||fetch(event.request)))});
