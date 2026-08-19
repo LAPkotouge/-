a9f2d21a72a1923f04995c080ecf1b841bd0b028
LAPkotouge <kotouge@lap.co.jp>
2026-08-19 23:25:19 +0900

    Bump PWA cache to v22

--- taikai_voice_pwa_v1/sw.js
+++ taikai_voice_pwa_v1/sw.js
@@ -1,4 +1,4 @@
-const CACHE = "taikai-voice-v21";
-self.addEventListener("install",event=>{event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(["./","./index.html","./style.css?v=21","./app/app.js?v=20","./manifest.json"])).then(()=>self.skipWaiting()))});
+const CACHE = "taikai-voice-v22";
+self.addEventListener("install",event=>{event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(["./","./index.html","./style.css?v=22","./app/app.js?v=22","./manifest.json"])).then(()=>self.skipWaiting()))});
 self.addEventListener("activate",event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key)))).then(()=>self.clients.claim()))});
 self.addEventListener("fetch",event=>{event.respondWith(caches.match(event.request).then(response=>response||fetch(event.request)))});
