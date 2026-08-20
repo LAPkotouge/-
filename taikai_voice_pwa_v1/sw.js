d19791f9f478c58c23a3bcb2f4915d7b0bf45ddd
LAPkotouge <kotouge@lap.co.jp>
2026-08-20 11:06:59 +0900

    Refresh service worker cache for V29 mobile CSS

--- taikai_voice_pwa_v1/sw.js
+++ taikai_voice_pwa_v1/sw.js
@@ -1,4 +1,4 @@
-const CACHE = "taikai-voice-v29-version2";
-self.addEventListener("install",event=>{event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(["./","./index.html","./style.css?v=29","./app/app.js?v=26","./manifest.json"])).then(()=>self.skipWaiting()))});
+const CACHE = "taikai-voice-v29-version3";
+self.addEventListener("install",event=>{event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(["./","./index.html","./style.css?v=29r3","./app/app.js?v=26","./manifest.json"])).then(()=>self.skipWaiting()))});
 self.addEventListener("activate",event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key)))).then(()=>self.clients.claim()))});
 self.addEventListener("fetch",event=>{event.respondWith(caches.match(event.request).then(response=>response||fetch(event.request)))});
