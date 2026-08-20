fd728038164eae8b986ac483bca00dffd273f787
LAPkotouge <kotouge@lap.co.jp>
2026-08-20 17:00:18 +0900

    V30: refresh cache for clear button styles

--- taikai_voice_pwa_v1/sw.js
+++ taikai_voice_pwa_v1/sw.js
@@ -1,4 +1,4 @@
-const CACHE = "taikai-voice-v30-version6";
+const CACHE = "taikai-voice-v30-version7";
 
 self.addEventListener("install", event => {
   event.waitUntil(
@@ -8,7 +8,7 @@ self.addEventListener("install", event => {
         "./index.html",
         "./style.css?v=29r3",
         "./app/app.js?v=26",
-        "./app/restore.js?v=30r6",
+        "./app/restore.js?v=30r7",
         "./app/reliable.js?v=30r5",
         "./manifest.json"
       ]))
