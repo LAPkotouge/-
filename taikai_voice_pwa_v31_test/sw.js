6e91a107596cb295d41d81279a804fc90ebd5383
LAPkotouge <kotouge@lap.co.jp>
2026-09-22 07:18:46 +0900

    Publish isolated V31 test build: sw.js

---
+++ taikai_voice_pwa_v31_test/sw.js
@@ -0,0 +1,33 @@
+const CACHE = "lap-number-v31-test1";
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE)
+      .then(cache => cache.addAll([
+        "./",
+        "./index.html",
+        "./style.css?v=29r3",
+        "./app/app.js?v=31d1",
+        "./app/restore.js?v=30r7",
+        "./app/reliable.js?v=31d1",
+        "./manifest.json"
+      ]))
+      .then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys()
+      .then(keys => Promise.all(
+        keys.filter(key => key !== CACHE).map(key => caches.delete(key))
+      ))
+      .then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response => response || fetch(event.request))
+  );
+});
