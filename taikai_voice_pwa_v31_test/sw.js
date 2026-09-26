44ce9d48344e2bceb8341d422d5b7e7d0699e020
LAPkotouge <kotouge@lap.co.jp>
2026-09-26 18:01:58 +0900

    Bump V31 V30-compatible transport test cache

--- taikai_voice_pwa_v31_test/sw.js
+++ taikai_voice_pwa_v31_test/sw.js
@@ -1,13 +1,13 @@
-const CACHE = "lap-number-v31-v7popup1";
+const CACHE = "lap-number-v31-v30transport1";
 
 self.addEventListener("install", event => {
   event.waitUntil(
     caches.open(CACHE)
       .then(cache => cache.addAll([
         "./style.css?v=29r3",
-        "./app/app.js?v=31v7popup1",
+        "./app/app.js?v=31v30transport1",
         "./app/restore.js?v=30r7",
-        "./app/reliable.js?v=31v7popup1",
+        "./app/reliable.js?v=31v30transport1",
         "./manifest.json"
       ]))
       .then(() => self.skipWaiting())
