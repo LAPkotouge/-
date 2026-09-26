fd07ffb1c30858d210a9d4122f9f0d9e2bf43de4
LAPkotouge <kotouge@lap.co.jp>
2026-09-26 13:12:04 +0900

    V31 refresh service worker cache

--- taikai_voice_pwa_v31_test/sw.js
+++ taikai_voice_pwa_v31_test/sw.js
@@ -1,4 +1,4 @@
-const CACHE = "lap-number-v31-test1";
+const CACHE = "lap-number-v31-test2";
 
 self.addEventListener("install", event => {
   event.waitUntil(
@@ -7,9 +7,9 @@ self.addEventListener("install", event => {
         "./",
         "./index.html",
         "./style.css?v=29r3",
-        "./app/app.js?v=31d1",
+        "./app/app.js?v=31d2",
         "./app/restore.js?v=30r7",
-        "./app/reliable.js?v=31d1",
+        "./app/reliable.js?v=31d2",
         "./manifest.json"
       ]))
       .then(() => self.skipWaiting())
